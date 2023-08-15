cb4f3ff1eacb85fa221d01d222978996f98358eb
eric <eric2043@gmail.com>
2023-08-15 00:43:40 +0000

    added script

---
+++ start-stream.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ssh_args="eric@192.168.1.3"
+
+check_host(){
+	result=1
+	while [[ $result -ne 0 ]]
+	do
+		echo "Checking host..."
+		ssh $ssh_args "exit 0" 2> /dev/null
+		result=$?
+		[[ $result -ne 0 ]] && {
+			echo "Failed to ssh to $ssh_args with exit code $result"
+		}
+		sleep 2
+	done
+	echo "Host is ready for streaming!"
+}
+
+start_stream(){
+	ssh -t $ssh_args "tmux new ~/scripts/sunshine.sh"
+}
+
+cleanup(){
+	ssh $ssh_args "pkill -ef sunshine"
+	ssh $ssh_args "pkill -ef X"
+}
+
+check_host
+start_stream
+# Doing ctrl + c will continue the script and activate the cleanup
+cleanup
+
