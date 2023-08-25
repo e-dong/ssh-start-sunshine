395434d729ef192326b5803bd90b5d7ba3b89b9a
Eric Dong <eric2043@gmail.com>
2023-08-24 21:13:12 -0400

    Cleaned up script

--- start-stream.sh
+++ start-stream.sh
@@ -3,22 +3,24 @@
 ssh_args="eric@192.168.1.3"
 
 check_ssh(){
-	result=1
-	while [[ $result -ne 0 ]]
-	do
-		echo "checking host..."
-		ssh $ssh_args "exit 0" 2>/dev/null
-		result=$?
-		[[ $result -ne 0 ]] && echo "Failed to ssh to $ssh_args, with exit code $result"
-		sleep 2
-	done
-	echo "Host is ready for streaming!"
+  result=1
+  while [[ $result -ne 0 ]]
+  do
+    echo "checking host..."
+	ssh $ssh_args "exit 0" 2>/dev/null
+	result=$?
+	[[ $result -ne 0 ]] && {
+        echo "Failed to ssh to $ssh_args, with exit code $result"
+    }
+    sleep 3
+  done
+  echo "Host is ready for streaming!"
 }
 
 start_stream(){
-	echo "Starting sunshine server on host..."
-	echo "Start moonlight on your client of choice"
-	ssh -f $ssh_args "~/scripts/sunshine.sh &" 
+  echo "Starting sunshine server on host..."
+  echo "Start moonlight on your client of choice"
+  ssh -f $ssh_args "~/scripts/sunshine.sh &" 
 }
 
 
