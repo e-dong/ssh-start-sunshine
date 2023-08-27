56687ad882c7254585d7df03ef7e5f37c68b2534
Eric Dong <eric2043@gmail.com>
2023-08-27 19:20:12 -0400

    refactored scripts; added a script to set custom resolution on host

--- host-config/sunshine.sh
+++ host-config/sunshine.sh
@@ -14,15 +14,23 @@ ps -e | grep X >/dev/null
   } || echo "X Server failed to start"
 } || echo "X Server already running"
 
+sleep 3
+
+# Startup any apps
+#steam &>/dev/null &
+#firefox &>/dev/null &
+#kdeconnect-app &>/dev/null &
+
 # Check if sunshine is already running
 ps -e | grep -e .*sunshine$ >/dev/null
 [[ ${?} -ne 0 ]] && {
   sudo ~/scripts/update-udev.sh
-  sleep 1
   echo "Starting Sunshine!"
   sunshine > /dev/null &
   [[ ${?} -eq 0 ]] && {
       echo "Sunshine started successfully"
   } || echo "Sunshine failed to start"
 } || echo "Sunshine is already running"
+
+sleep 1
 
