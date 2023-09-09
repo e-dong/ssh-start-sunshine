93d5beb7ec5e6e4cb35f6670438d5b9f14d5400d
Eric Dong <eric2043@gmail.com>
2023-09-08 21:09:17 -0400

    Added script to set custom resolution

---
+++ host-config/set-custom-res.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+width=${1:-1920}
+height=${2:-1080}
+refresh_rate=120
+display_output="DP-0"
+# Reset scaling
+xrandr --output ${display_output} --scale 1
+
+mode_alias="${width}x${height}"
+
+modeline=$(cvt ${width} ${height} ${refresh_rate} | awk 'FNR == 2')
+xrandr_mode_str=${modeline//Modeline \"*\" /}
+#mode_name=$(echo ${modeline} | awk '{ print $2 }')
+
+echo "xrandr setting new mode ${mode_alias} ${xrandr_mode_str}"
+
+xrandr --newmode ${mode_alias} ${xrandr_mode_str}
+xrandr --addmode ${display_output} ${mode_alias}
+
+xrandr --output ${display_output} --primary --mode ${mode_alias} --pos 0x0 --rotate normal --scale 0.5
+$HOME/scripts/wallpaper.sh
+
--- host-config/sunshine.sh
+++ host-config/sunshine.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 export DISPLAY=:0
-#tmux new-session -d "startx -- $DISPLAY"
-#startx -- -dpi 144 &
+
+width=${1:-1920}
+height=${2:-1080}
 
 # Check existing X server
 ps -e | grep X >/dev/null
-[[ ${?} -ne 0 ]] && {
+[[ $? -ne 0 ]] && {
   echo "Starting X Server"
   startx &>/dev/null &
-  [[ ${?} -eq 0 ]] && {
+  [[ $? -eq 0 ]] && {
       echo "X Server started successfully"
   } || echo "X Server failed to start"
 } || echo "X Server already running"
 
 sleep 3
+$HOME/scripts/set-custom-res.sh ${width} ${height}
+#$HOME/scripts/wallpaper.sh
 
 # Startup any apps
 #steam &>/dev/null &
@@ -23,14 +26,12 @@ sleep 3
 
 # Check if sunshine is already running
 ps -e | grep -e .*sunshine$ >/dev/null
-[[ ${?} -ne 0 ]] && {
-  sudo ~/scripts/update-udev.sh
+[[ $? -ne 0 ]] && {
+  sudo ${HOME}/scripts/sunshine-setup.sh
   echo "Starting Sunshine!"
   sunshine > /dev/null &
-  [[ ${?} -eq 0 ]] && {
+  [[ $? -eq 0 ]] && {
       echo "Sunshine started successfully"
   } || echo "Sunshine failed to start"
 } || echo "Sunshine is already running"
-
-sleep 1
 
